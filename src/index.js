520ad5bfc9f2e79bb4f99fc965944d79902888e5
John Clarence Palma <johnclarencepalma7@gmail.com>
2026-01-18 22:45:58 +0800

    update: colon, line 185

--- src/index.js
+++ src/index.js
@@ -182,7 +182,7 @@ function CoWrite() {
 
 //render to show on page.
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<React.StrictMode><App /></React.StrictMode>)
+root.render(<React.StrictMode><App /></React.StrictMode>);
 
 
 
